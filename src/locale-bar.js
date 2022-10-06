0ba06a4733196ece05f29314b80e783ad97b4d4b
Igor J Marques <igorjrd2@gmail.com>
2022-10-05 22:02:35 -0300

    add statusbar tooltip text and minor changes

--- src/locale-bar.js
+++ src/locale-bar.js
@@ -1,10 +1,12 @@
 const vscode = require('vscode');
+const dict = require('./nls')
 
 module.exports = class LocaleBar{
     constructor(extensionPath){
         this.extensionPath = extensionPath;
         this.bar = vscode.window.createStatusBarItem(2, 10000);
-        this.bar.text = "$(ports-open-browser-icon)"
-        this.bar.command = 'cslPreview.chooseLocale'
+        this.bar.text = "$(ports-open-browser-icon)";
+        this.bar.command = 'cslPreview.chooseLocale';
+        this.bar.tooltip = dict['cslPreview.chooseLocale.title'];
     }
 }
